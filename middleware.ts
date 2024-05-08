4b3de0cf311086e973ab5d669400c3d35452e2b3
Leon <tahjib.leon399@gmail.com>
2024-05-08 20:43:52 +0600

    clerk implementation

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isDashboardRoute = createRouteMatcher(['/ask-question(.*)']);
+
+export default clerkMiddleware((auth, req) => {
+
+    // Restrict dashboard routes to signed in users
+    if (isDashboardRoute(req)) auth().protect();
+});
+
+export const config = {
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
