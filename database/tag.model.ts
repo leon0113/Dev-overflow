45426fef807683e4237f079cea5b5482ddbafcb8
Leon <tahjib.leon399@gmail.com>
2024-06-03 23:44:42 +0600

    created db models

---
+++ database/tag.model.ts
@@ -0,0 +1,40 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface ITag extends Document {
+    name: string,
+    description: string,
+    questions: Schema.Types.ObjectId[];
+    followers: Schema.Types.ObjectId[];
+    createdOn: Date;
+}
+
+const TagSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+    },
+    questions: [{
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+        // required: true,
+    }],
+    followers: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        // required: true,
+    }],
+    createdOn: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+
+const Tag = models.Tag || model('Tag', TagSchema); // we are checking first if the model already exists or not. if not then creating a new model
+
+
+export default Tag;
